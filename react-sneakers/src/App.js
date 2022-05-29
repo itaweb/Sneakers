07f133db0ee0e4cf16f5ea1b381c6127c51218d3
Сергей Щикунов <admod32@gmail.com>
2022-05-29 20:41:04 +0300

    Add code in css file

--- react-sneakers/src/App.js
+++ react-sneakers/src/App.js
@@ -1,5 +1,29 @@
 function App() {
-  return <div className="App">Hello, World!</div>;
+  return (
+    <div className="wrapper">
+      <header>
+        <div className="headerLeft">
+          <img src="/img/logo.png" width={40} height={40} />
+          <div className="headerInfo">
+            <h3>React Sneakers</h3>
+            <p>Магазин лучших кроссовок</p>
+          </div>
+        </div>
+        <ul className="headerRight">
+          <li>
+            <img src="/img/cart.svg" width={18} height={17.18} />
+            <span>1205 руб.</span>
+          </li>
+          <li>
+            <img src="/img/user.svg" width={20} height={20} />
+          </li>
+        </ul>
+      </header>
+      <div className="content">
+        <h1>Все кроссовки</h1>
+      </div>
+    </div>
+  );
 }
 
 export default App;
