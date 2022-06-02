2a9c1298007bec34c88ee218d6e36af38442d188
Сергей Щикунов <admod32@gmail.com>
2022-06-02 12:12:04 +0300

    Fin project

--- react-sneakers/src/App.js
+++ react-sneakers/src/App.js
@@ -1,21 +1,36 @@
 function App() {
   return (
-    <div className="wrapper">
-      <header>
-        <div className="headerLeft">
-          <img src="/img/logo.png" width={40} height={40} />
-          <div className="headerInfo">
+    <div className="wrapper clear">
+      <header className="d-flex justify-between">
+        <div className="d-flex align-center">
+          <img
+            src="/img/logo.png"
+            width={40}
+            height={40}
+            alt="Это картинка лого"
+          />
+          <div>
             <h3>React Sneakers</h3>
             <p>Магазин лучших кроссовок</p>
           </div>
         </div>
-        <ul className="headerRight">
-          <li>
-            <img src="/img/cart.svg" width={18} height={17.18} />
+        <ul className="d-flex">
+          <li className="mr-30">
+            <img
+              src="/img/cart.svg"
+              width={18}
+              height={17.18}
+              alt="Иконка корзины"
+            />
             <span>1205 руб.</span>
           </li>
           <li>
-            <img src="/img/user.svg" width={20} height={20} />
+            <img
+              src="/img/user.svg"
+              width={20}
+              height={20}
+              alt="Иконка личного кабинета"
+            />
           </li>
         </ul>
       </header>
